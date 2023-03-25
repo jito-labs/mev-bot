e9fd771afbd5043e2ac46e403313b73bc8b0bfae
futurekarol <29566494+futurekarol@users.noreply.github.com>
2023-03-25 13:02:01 +0000

    add whirlpools program to mempool listener

--- src/mempool.ts
+++ src/mempool.ts
@@ -11,6 +11,7 @@ const AUTH_KEYPAIR_PATH = config.get('auth_keypair_path');
 const PROGRAMS_OF_INTEREST = [
   new PublicKey('JUP4Fb2cqiRUcaTHdrPC8h2gNsA2ETXiPDD33WcGuJB'), // Jupiter
   new PublicKey('675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8'), // Raydium 
+  new PublicKey('whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc'), // Orca Whirlpools
 ];
 
 const decodedKey = new Uint8Array(
