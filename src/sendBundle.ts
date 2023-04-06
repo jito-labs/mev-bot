ec6d7387e938b4bf7202bf096e86313d3cfeda3e
futurekarol <29566494+futurekarol@users.noreply.github.com>
2023-04-06 17:11:15 +0000

    log actual txn hash when sending bundle

--- src/sendBundle.ts
+++ src/sendBundle.ts
@@ -1,10 +1,8 @@
 import { Bundle } from './buildBundle.js';
-import { Timings } from './types.js';
 import { searcherClient } from './jitoClient.js';
 import { logger } from './logger.js';
 import { Bundle as JitoBundle } from 'jito-ts/dist/sdk/block-engine/types.js';
-
-const sentBundleTimings: Map<string, Timings> = new Map();
+import bs58 from 'bs58';
 
 async function sendBundle(
   bundleIterator: AsyncGenerator<Bundle>,
@@ -14,26 +12,6 @@ async function sendBundle(
       const bundleId = bundleResult.bundleId;
       const isAccepted = bundleResult.accepted;
       const isRejected = bundleResult.rejected;
-      const timings = sentBundleTimings.get(bundleId);
-
-      // edge case this runs before the timings are set due to promise scheduling
-      if (!timings) {
-        logger.info(
-          `chain timings: pre sim: ${
-            timings.preSimEnd - timings.mempoolEnd
-          }ms, sim: ${timings.simEnd - timings.preSimEnd}ms, post sim: ${
-            timings.postSimEnd - timings.simEnd
-          }ms, arb calc: ${
-            timings.calcArbEnd - timings.postSimEnd
-          }ms, build bundle: ${
-            timings.buildBundleEnd - timings.calcArbEnd
-          }ms send bundle: ${
-            timings.bundleSent - timings.buildBundleEnd
-          }ms ::: total ${timings.bundleSent - timings.mempoolEnd}ms (${
-            Date.now() - timings.bundleSent
-          }ms to get result)`,
-        );
-      }
       if (isAccepted) {
         logger.info(
           `Bundle ${bundleId} accepted in slot ${bundleResult.accepted.slot}`,
@@ -53,17 +31,23 @@ async function sendBundle(
   for await (const { bundle, timings } of bundleIterator) {
     searcherClient.sendBundle(new JitoBundle(bundle, 5)).then((bundleId) => {
       logger.info(
-        `Bundle ${bundleId} sent, backrunning ${bundle[0].signatures}`,
+        `Bundle ${bundleId} sent, backrunning ${bs58.encode(
+          bundle[0].signatures[0],
+        )}`,
       );
-      sentBundleTimings.set(bundleId, {
-        mempoolEnd: timings.mempoolEnd,
-        preSimEnd: timings.preSimEnd,
-        simEnd: timings.simEnd,
-        postSimEnd: timings.postSimEnd,
-        calcArbEnd: timings.calcArbEnd,
-        buildBundleEnd: timings.buildBundleEnd,
-        bundleSent: Date.now(),
-      });
+      logger.info(
+        `chain timings: pre sim: ${
+          timings.preSimEnd - timings.mempoolEnd
+        }ms, sim: ${timings.simEnd - timings.preSimEnd}ms, post sim: ${
+          timings.postSimEnd - timings.simEnd
+        }ms, arb calc: ${
+          timings.calcArbEnd - timings.postSimEnd
+        }ms, build bundle: ${
+          timings.buildBundleEnd - timings.calcArbEnd
+        }ms send bundle: ${Date.now() - timings.buildBundleEnd}ms ::: total ${
+          Date.now() - timings.mempoolEnd
+        }ms`,
+      );
     });
   }
 }
