540f59f318bfa6eb3c35d7af6a38f9a248fac55d
futurekarol <29566494+futurekarol@users.noreply.github.com>
2023-04-20 13:07:16 +0000

    add orca v1 to mempool

--- src/mempool.ts
+++ src/mempool.ts
@@ -8,7 +8,8 @@ const PROGRAMS_OF_INTEREST = [
   new PublicKey('675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8'), // Raydium
   new PublicKey('whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc'), // Orca Whirlpools
   new PublicKey('CAMMCzo5YL8w4VFF8KVHrK22GGUsp5VTaW7grrKgrWqK'), // Raydium CLMM
-  new PublicKey('DjVE6JNiYqPL2QXyCUUh8rNjHrbz9hXHNYt99MQ59qw1'), // Orca V2
+  new PublicKey('9W959DqEETiGZocYWCQPaJ6sBmUzgfxXfqGeTEdp3aQP'), // Orca V2
+  new PublicKey('DjVE6JNiYqPL2QXyCUUh8rNjHrbz9hXHNYt99MQ59qw1'), // Orca
 ];
 
 type MempoolUpdate = {
